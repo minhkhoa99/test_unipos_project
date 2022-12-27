7d812aaaf5ed70e13f51dafd1f204185fdbdee6d
minhkhoa99 <110026964+minhkhoa99@users.noreply.github.com>
2022-12-27 14:20:40 +0700

    commits

--- webapp/src/pages/widgets/PostsWidget.jsx
+++ webapp/src/pages/widgets/PostsWidget.jsx
@@ -3,54 +3,53 @@ import { useDispatch, useSelector } from "react-redux";
 import { setPosts } from "../../state/index";
 import PostWidget from "./PostWidget";
 
-
 const PostsWidget = ({ userId, isProfile = false }) => {
   const dispatch = useDispatch();
   const posts = useSelector((state) => state.posts);
   console.log(posts);
   const token = useSelector((state) => state.token);
 
-const PostsWidget = (data) => {
-  // console.log(data);
-  // const dispatch = useDispatch();
-  // const posts = useSelector((state) => state.posts);
-  // const token = useSelector((state) => state.token);
+  const PostsWidget = (data) => {
+    // console.log(data);
+    // const dispatch = useDispatch();
+    // const posts = useSelector((state) => state.posts);
+    // const token = useSelector((state) => state.token);
 
+    // const getPosts = async () => {
+    //   const response = await fetch("http://localhost:3001/posts", {
+    //     method: "GET",
+    //     headers: { Authorization: `Bearer ${token}` },
+    //   });
+    //   const data = await response.json();
+    //   dispatch(setPosts({ posts: data }));
+    // };
 
-  // const getPosts = async () => {
-  //   const response = await fetch("http://localhost:3001/posts", {
-  //     method: "GET",
-  //     headers: { Authorization: `Bearer ${token}` },
-  //   });
-  //   const data = await response.json();
-  //   dispatch(setPosts({ posts: data }));
-  // };
+    // const getUserPosts = async () => {
+    //   const response = await fetch(
+    //     `http://localhost:3001/posts/${userId}/posts`,
+    //     {
+    //       method: "GET",
+    //       headers: { Authorization: `Bearer ${token}` },
+    //     }
+    //   );
+    //   const data = await response.json();
+    //   dispatch(setPosts({ posts: data }));
+    // };
 
-  // const getUserPosts = async () => {
-  //   const response = await fetch(
-  //     `http://localhost:3001/posts/${userId}/posts`,
-  //     {
-  //       method: "GET",
-  //       headers: { Authorization: `Bearer ${token}` },
-  //     }
-  //   );
-  //   const data = await response.json();
-  //   dispatch(setPosts({ posts: data }));
-  // };
+    // useEffect(() => {
+    //   if (isProfile) {
+    //     getUserPosts();
+    //   } else {
+    //     getPosts();
+    //   }
+    // }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-  // useEffect(() => {
-  //   if (isProfile) {
-  //     getUserPosts();
-  //   } else {
-  //     getPosts();
-  //   }
-  // }, []); // eslint-disable-line react-hooks/exhaustive-deps
-
-  return (
-    <>
-      <PostWidget/>
-    </>
-  );
+    return (
+      <>
+        <PostWidget />
+      </>
+    );
+  };
 };
 
 export default PostsWidget;
