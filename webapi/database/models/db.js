0497398575e0dc5b813aed66ea37bc29d1e06273
thong1403 <110025148+thong1403@users.noreply.github.com>
2023-01-16 19:09:08 +0700

    update

--- webapi/database/models/db.js
+++ webapi/database/models/db.js
@@ -1,8 +1,8 @@
 module.exports = {
   HOST: "localhost",
   USER: "root",
-  PASSWORD: "Yeulam0909",
-  DATABASE: "unipos_project",
+  PASSWORD: "12345678",
+  DATABASE: "unipost_project",
 
   DIALECT: "mysql",
 };
